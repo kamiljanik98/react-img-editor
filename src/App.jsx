a3172dccfc4ef2b9917c38dd0ce722cb2a702c38
yaneczq <yaneczq1@gmail.com>
2024-10-19 13:43:14 +0200

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,221 @@
-import { useState } from 'react';
-import ImageCanvas from './components/ImageCanvas';
-import DropzoneComponent from './components/DropzoneComponent';
+import ImageCanvas from "./components/ImageCanvas";
+import { useDropzone } from "react-dropzone";
+import { useState, useEffect, useRef } from "react";
+import { FiList, FiSliders, FiCornerDownLeft, FiUpload } from "react-icons/fi";
+import {
+  cleanupObjectURL,
+  saveImagesToLocalStorage,
+  loadImagesFromLocalStorage,
+  defStyle,
+} from "./utils/utils";
 
-function App() {
+const App = () => {
+  const [filter, setFilter] = useState(
+    defStyle.light.concat(defStyle.color, defStyle.blur),
+  );
+  const canvasRef = useRef(null);
+  const [acceptedFiles, setAcceptedFiles] = useState([]);
   const [imageSrc, setImageSrc] = useState(null);
-  const [filters, setFilters] = useState({
-    blur: 0,
-    brightness: 100,
-    contrast: 100,
-    saturation: 100,
-  });
+  const [isFiltersOpen, setIsFiltersOpen] = useState(false);
+  const [isFileListOpen, setIsFileListOpen] = useState(false);
 
-  const handleFilterChange = (e) => {
-    const { name, value } = e.target;
-    setFilters({
-      ...filters,
-      [name]: value,
+  useEffect(() => {
+    loadImagesFromLocalStorage()
+      .then((filesArray) => {
+        setAcceptedFiles(filesArray);
+        if (filesArray[0]) setImageSrc(URL.createObjectURL(filesArray[0]));
+      })
+      .catch((error) => console.error("Error loading saved images:", error));
+
+    cleanupObjectURL();
+  }, []);
+
+  const onDrop = (files) => {
+    const newFiles = files.filter(
+      (file) =>
+        !acceptedFiles.some(
+          (existingFile) =>
+            existingFile.name === file.name && existingFile.size === file.size,
+        ),
+    );
+  
+    if (newFiles.length === 0) {
+      alert("No new files to add. Please check for duplicates.");
+      return;
+    }
+  
+    setAcceptedFiles((prevFiles) => {
+      const updatedFiles = [...prevFiles, ...newFiles];
+      saveImagesToLocalStorage(updatedFiles);
+      return updatedFiles;
+    });
+  
+    if (newFiles[0]) {
+      setImageSrc(URL.createObjectURL(newFiles[0]));
+    }
+  
+    alert("Success!");
+  };
+
+  const onDropRejected = (rejectedFiles) => {
+    rejectedFiles.forEach(({ file, errors }) => {
+      errors.forEach((err) => {
+        if (err.code === "file-invalid-type") {
+          alert(
+            `Error: File "${file.name}" is not a valid format. Please upload a .jpg or .png file.`,
+          );
+        }
+      });
     });
   };
 
+  const { getInputProps, getRootProps } = useDropzone({
+    noKeyboard: true,
+    multiple: 2,
+    accept: { "image/png": [".png"], "image/jpg": [".jpg"] },
+    onDropRejected,
+    onDrop,
+  });
+
+  const onFilterChange = (e) => {
+    const filterId = filter.findIndex((f) => f.name === e.target.name);
+    if (filterId !== -1) {
+      setFilter((prev) => {
+        const newFilter = [...prev];
+        newFilter[filterId].value = Number(e.target.value);
+        return newFilter;
+      });
+    }
+  };
+
+  const acceptedFileItems = acceptedFiles.map((file) => (
+    <li
+      className="list__item"
+      key={`${file.name}-${file.size}-${file.lastModified}`} // Unique key
+      onClick={() => setImageSrc(URL.createObjectURL(file))}
+      style={{ cursor: "pointer" }}
+    >
+      <p>{file.name}</p>
+      <p>{file.size} bytes</p>
+    </li>
+  ));
+
+  const togglePanel = (setPanelState) => () => setPanelState((prev) => !prev);
+
   return (
-    <div className="App">
-      <DropzoneComponent setImageSrc={setImageSrc} />
-      
-      {imageSrc && (
+    <div className="app">
+      <nav className="navbar">
+        <div {...getRootProps({ className: "dropzone" })}>
+          <input {...getInputProps()} />
+          <FiUpload size={24} />
+        </div>
+        <div className="menu-icon" onClick={togglePanel(setIsFiltersOpen)}>
+          {isFiltersOpen ? <FiCornerDownLeft /> : <FiSliders />}
+        </div>
+        <div className="menu-icon" onClick={togglePanel(setIsFileListOpen)}>
+          {isFileListOpen ? <FiCornerDownLeft /> : <FiList />}
+        </div>
+      </nav>
+  
+      {imageSrc ? (
         <>
-          <ImageCanvas imageSrc={imageSrc} filters={filters} />
+          <ImageCanvas
+            filter={filter}
+            imageSrc={imageSrc}
+            canvasRef={canvasRef}
+          />
 
-          <div className="controls">
-            <label>
-              Blur: 
-              <input 
-                type="range" 
-                name="blur" 
-                min="0" 
-                max="10" 
-                value={filters.blur} 
-                onChange={handleFilterChange} 
-              />
-            </label>
+          {/* Display filters only after image is loaded */}
+          {isFiltersOpen && (
+            <div className="filters-overlay">
+              <div className="app__header">
+                <h1>CSS Filters</h1>
+                <p>Use sliders below to edit your image...</p>
+              </div>
 
-            <label>
-              Brightness: 
-              <input 
-                type="range" 
-                name="brightness" 
-                min="0" 
-                max="200" 
-                value={filters.brightness} 
-                onChange={handleFilterChange} 
-              />
-            </label>
-
-            <label>
-              Contrast: 
-              <input 
-                type="range" 
-                name="contrast" 
-                min="0" 
-                max="200" 
-                value={filters.contrast} 
-                onChange={handleFilterChange} 
-              />
-            </label>
-
-            <label>
-              Saturation: 
-              <input 
-                type="range" 
-                name="saturation" 
-                min="0" 
-                max="200" 
-                value={filters.saturation} 
-                onChange={handleFilterChange} 
-              />
-            </label>
-          </div>
+              <div className="slider__container">
+                <h3>Light</h3>
+                {defStyle.light.map((x, index) => (
+                  <div className="slider" key={index}>
+                    <div className="slider-header">
+                      <label htmlFor={`customRange${index}`}>{x.name}</label>
+                      <p>{x.value}%</p>
+                    </div>
+                    <input
+                      type="range"
+                      id={`customRange${index}`}
+                      value={x.value}
+                      min="0"
+                      max="100"
+                      name={x.name}
+                      onChange={onFilterChange}
+                    />
+                  </div>
+                ))}
+              </div>
+              <div className="slider__container">
+                <h3>Color</h3>
+                {defStyle.color.map((x, index) => (
+                  <div className="slider" key={index}>
+                    <div className="slider-header">
+                      <label htmlFor={`customRange${index}`}>{x.name}</label>
+                      <p>{x.value}%</p>
+                    </div>
+                    <input
+                      type="range"
+                      id={`customRange${index}`}
+                      value={x.value}
+                      min="0"
+                      max="100"
+                      name={x.name}
+                      onChange={onFilterChange}
+                    />
+                  </div>
+                ))}
+              </div>
+              <div className="slider__container">
+                <h3>Blur</h3>
+                {defStyle.blur.map((x, index) => (
+                  <div className="slider" key={index}>
+                    <div className="slider-header">
+                      <label htmlFor={`customRange${index}`}>{x.name}</label>
+                      <p>{x.value}%</p>
+                    </div>
+                    <input
+                      type="range"
+                      id={`customRange${index}`}
+                      value={x.value}
+                      min="0"
+                      max="100"
+                      name={x.name}
+                      onChange={onFilterChange}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
         </>
+      ) : (
+        <div {...getRootProps({ className: "empty__dropzone" })}>
+          <div className="status__message">
+            <p>No Files Uploaded</p>
+            <em>Max 5MB - total upload limit</em>
+          </div>
+        </div>
+      )}
+
+      {isFileListOpen && (
+        <div className="filelist-overlay">
+          <div className="app__header">
+            <h1>Browse your files</h1>
+            <p>Please select an image...</p>
+          </div>
+          <div className="list">{acceptedFileItems}</div>
+        </div>
       )}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/ImageCanvas.jsx
+++ src/components/ImageCanvas.jsx
@@ -1,23 +1,151 @@
-import { useRef, useEffect } from 'react';
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { FiZoomIn, FiZoomOut, FiDownloadCloud, FiTrash2 } from "react-icons/fi";
+import { downloadFilteredImage, resetStorageAndReload } from "../utils/utils";
 
-const ImageCanvas = ({ imageSrc, filters }) => {
-  const canvasRef = useRef(null);
+const ImageCanvas = ({ imageSrc, filter = [], canvasRef }) => {
+  const [zoom, setZoom] = useState(0.5); // Zoom state
 
   useEffect(() => {
-    if (imageSrc) {
-      const canvas = canvasRef.current;
-      const context = canvas.getContext('2d');
-      const image = new Image();
-      image.onload = () => {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.filter = `blur(${filters.blur}px) brightness(${filters.brightness}%) contrast(${filters.contrast}%) saturate(${filters.saturation}%)`;
-        context.drawImage(image, 0, 0, canvas.width, canvas.height);
-      };
-      image.src = imageSrc;
-    }
-  }, [imageSrc, filters]);
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const ctx = canvas.getContext("2d");
+    const image = new Image();
+    const resizeCanvas = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+      // Adjust canvas for high-DPI displays
+      const scale = window.devicePixelRatio || 1;
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+  
+      // Set canvas dimensions for high-DPI
+      
+      // Check if the computed canvas width is less than 842px
+      if (width < 842) {
+        canvas.width = (width / 2) * scale;
+        canvas.height = (height / 2) * scale;
+        canvas.style.width = `${width / 1}px`; // Reduce width by half
+        canvas.style.height = `${height / 1}px`; // Reduce height by half
+      } else {
+        // Otherwise, set to full width and height
+          canvas.width = width * scale;
+          canvas.height = height * scale;
+          canvas.style.width = `${width}px`;
+          canvas.style.height = `${height}px`;
+      }
+  
+      // Draw the image onto the canvas
+      drawImage(ctx, image);
+  };
+  
 
-  return <canvas ref={canvasRef} width={400} height={400} style={{ border: '1px solid black' }} />;
+    const drawImage = (ctx, image) => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      const filterString = filter
+        .map((f) => {
+          const { name, value } = f;
+          switch (name) {
+            case "Grayscale":
+              return `grayscale(${value}%)`;
+            case "Sepia":
+              return `sepia(${value}%)`;
+            case "Opacity":
+              return `opacity(${value / 100})`;
+            case "Invert":
+              return `invert(${value}%)`;
+            case "Hue-Rotate":
+              return `hue-rotate(${value}deg)`;
+            case "Brightness":
+              return `brightness(${value}%)`;
+            case "Contrast":
+              return `contrast(${value}%)`;
+            case "Saturate":
+              return `saturate(${value * 4}%)`;
+            case "Blur":
+              return `blur(${value}px)`;
+            default:
+              return "";
+          }
+        })
+        .join(" ");
+
+      ctx.filter = filterString;
+
+      // Calculate zoomed image dimensions
+
+      // Translate the canvas context to center before scaling
+      const centerX = canvas.width / 2;
+      const centerY = canvas.height / 2;
+
+      ctx.save(); // Save current state of canvas
+      ctx.translate(centerX, centerY); // Move to the center of the canvas
+      ctx.scale(zoom, zoom); // Apply zoom scale
+      ctx.translate(-image.width / 2, -image.height / 2); // Translate back by half of the image dimensions
+
+      ctx.drawImage(image, 0, 0, image.width, image.height); // Draw image at the center
+
+      ctx.restore(); // Restore the original state of the canvas
+    };
+
+    image.onload = () => {
+      resizeCanvas();
+      drawImage(ctx, image);
+    };
+
+    image.src = imageSrc;
+
+    window.addEventListener("resize", resizeCanvas);
+
+    return () => {
+      window.removeEventListener("resize", resizeCanvas);
+    };
+  }, [imageSrc, filter, zoom, canvasRef]);
+
+  return (
+    <>
+      {/* Zoom controls */}
+      <div className="controls">
+        <button
+          className="zoom-button zoom-in"
+          onClick={() => setZoom(zoom + 0.1)}
+        >
+          <FiZoomIn size={16} />
+        </button>
+        <button
+          className="zoom-button zoom-out"
+          onClick={() => setZoom(zoom - 0.1)}
+          disabled={zoom <= 0.2}
+        >
+          <FiZoomOut size={16} />
+        </button>
+        <button
+          className="download-button"
+          onClick={() => downloadFilteredImage(canvasRef)}
+        >
+          <FiDownloadCloud size={16} />
+        </button>
+        <button onClick={resetStorageAndReload}>
+          <FiTrash2 size={16} />
+        </button>
+      </div>
+
+      {/* Canvas */}
+      <canvas ref={canvasRef} />
+    </>
+  );
+};
+
+ImageCanvas.propTypes = {
+  imageSrc: PropTypes.string.isRequired,
+  filter: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      value: PropTypes.number.isRequired,
+    }),
+  ),
+  canvasRef: PropTypes.object.isRequired,
 };
 
 export default ImageCanvas;
